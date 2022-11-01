492e7776401ff7b1e570d0eb6373ee29e93a7673
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2022-11-01 21:54:13 +0800

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Objects/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Objects
+{
+    class BaseObject
+    {
+        public float x;
+        public float y;
+        public float Angle;
+        public BaseObject(float x, float y, float angle)
+        {
+            this.x = x;
+            this.y = y;
+            Angle = angle;
+        }
+        public virtual void Render(Graphics g)
+        {
+
+        }
+    }
+}
---
+++ WinFormsApp1/Objects/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Objects
+{
+    class MyRectangle : BaseObject
+    {
+        public MyRectangle(float x, float y, float angle)
+        {
+
+        }
+        public override void Render(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Green), 200, 100, 50, 30);
+            g.DrawRectangle(new Pen(Color.Red), 200, 100, 50, 30);
+        }
+    }
+}
